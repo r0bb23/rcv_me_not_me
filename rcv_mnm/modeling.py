1cf87e320e3dbac3c33ec34185f2c232d0c94f6d
Robert Beatty <r0bb2323@gmail.com>
2022-01-06 17:27:33 -0500

    fine tuning.

--- rcv_mnm/modeling.py
+++ rcv_mnm/modeling.py
@@ -8,9 +8,10 @@ import tensorflow as tf
 
 @st.cache(allow_output_mutation=True)
 def load_model(
-    model_path: str = "models/latest_finetuned_mnm_model.tflite",
+    model_path: str = "models/latest_finetuned_mnm_model.h5",
 ):
-    model = tf.keras.models.load_model(model_path)
+    export_path = os.path.join(os.getcwd(), model_path)
+    model = tf.keras.models.load_model(export_path)
     return model
 
 @st.cache()
